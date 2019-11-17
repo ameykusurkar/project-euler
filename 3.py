83b26af8b5004d92814d8befab65e05a0e8b9bb0
Amey Kusurkar <amey1000@gmail.com>
2019-11-17 22:33:34 +0000

    Some refactoring

--- 3.py
+++ 3.py
@@ -1,22 +1,15 @@
-import math
+from math import sqrt
 
 def largest_prime_factor(n):
-    sqrt_n = int(math.sqrt(n))
+    sqrt_n = int(sqrt(n))
     for i in reversed(range(sqrt_n)):
         if prime_factor(n, i):
             return i
 
 def prime_factor(n, i):
-    return factor(n, i) and is_prime(i)
-
-def factor(n, i):
-    return n % i == 0
+    return n % i == 0 and is_prime(i)
 
 def is_prime(i):
-    for attempt in range(2, int(math.sqrt(i)) + 1):
-        if i % attempt == 0:
-            return False
-
-    return True
+    return not any(i % attempt == 0 for attempt in range(2, int(sqrt(i)) + 1))
 
 print(largest_prime_factor(600851475143))
